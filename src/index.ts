d521b2009080d83af8b3fd66330af2d70b79f787
PabloDeAlbu <pablo.cda@gmail.com>
2020-06-22 12:23:41 -0300

    purge de index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,3 @@ const observer: Observer<any> = {
     complete: () => console.info('completado')
 };
 
-const obs$ = new Observable<string>( subs => {
-
-    subs.next(String(Date.now()));
-    subs.next('Mundo!');
-
-    subs.complete();
-
-});
-
-//obs$.subscribe(console.log);
-
-//obs$.subscribe( resp => console.log ('Print: '+ resp))
-
-obs$.subscribe(observer);
