cd63f767b4c29271b73af9b9da28d4a41e7475e4
PabloDeAlbu <pablo.cda@gmail.com>
2020-06-23 11:10:33 -0300

    listo subject

---
+++ src/observables/03-subject.ts
@@ -0,0 +1,43 @@
+import {Observable, Subscriber, Observer, Subject, Subscription} from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('next: ', value),
+    error: error => console.warn('error: ', error),
+    complete: () => console.info('completado')
+};
+
+const intervalo$ = new Observable<number>(subs => {
+
+    const intervalID = setInterval(() => {
+        subs.next(Math.random())
+    },1000)
+
+    return() => {
+        clearInterval(intervalID);
+        console.log('Intervalo destruido')
+    };
+});
+
+/**
+ * 1 - Casteo múltiple
+ * 2 - También es un Observer
+ * 3 - Next, error y complete
+ */
+const subject$ = new Subject(); 
+
+const suscription = intervalo$.subscribe(subject$);
+// const subs1 = intervalo$.subscribe(rnd => console.log('subs1', rnd));
+// const subs2 = intervalo$.subscribe(rnd => console.log('subs2', rnd));
+
+const subs1 = subject$.subscribe(observer);
+const subs2 = subject$.subscribe(observer);
+
+setTimeout( ()=> {
+
+    subject$.next(10);
+    subject$.complete();
+    console.log('completado');
+    suscription.unsubscribe();
+    
+
+},3500)
