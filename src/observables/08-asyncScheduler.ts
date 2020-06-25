69808cec6ae7a58ea2aee1d746001d902eef51d5
PabloDeAlbu <pablo.cda@gmail.com>
2020-06-25 12:01:46 -0300

    asyncScheduler

---
+++ src/observables/08-asyncScheduler.ts
@@ -0,0 +1,24 @@
+import { asyncScheduler } from 'rxjs'
+
+ const saludar = () => console.log('Hola mundo');
+ const saludar2 = nombre => console.log(`Hola ${ nombre }`);
+
+//  asyncScheduler.schedule(saludar, 2000);
+//  asyncScheduler.schedule(saludar2, 2000, 'Pablo');
+
+
+
+// setInterval( () => {}, 3000);
+const subs = asyncScheduler.schedule( function(state){
+
+    console.log('state', state);
+    this.schedule( state + 1, 1000);
+ 
+}, 3000, 0 )
+
+
+// setTimeout( () => {
+//     subs.unsubscribe();
+// },6000)
+
+asyncScheduler.schedule( () => subs.unsubscribe(), 6000 );
